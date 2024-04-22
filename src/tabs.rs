72430d91121fa3a4cc31cdffa95f9ef227903335
James Pharaoh <james@pharaoh.uk>
2024-04-22 14:34:37 +0000

    rename Tabs to BoxTabs and add SlimTabs

--- src/tabs.rs
+++ src/tabs.rs
@@ -4,17 +4,17 @@ use std::iter;
 use crate::{ Attr, Printable, Target };
 use crate::border_box::BoxStyle;
 
-pub struct Tabs <'dat> {
+pub struct BoxTabs <'dat> {
 	tabs: Vec <(Cow <'dat, str>, BoxStyle)>,
 }
 
-impl <'dat> Tabs <'dat> {
+impl <'dat> BoxTabs <'dat> {
 	pub fn new (tabs: Vec <(Cow <'dat, str>, BoxStyle)>) -> Self {
 		Self { tabs }
 	}
 }
 
-impl <'tar> Printable <'tar> for Tabs <'tar> {
+impl <'tar> Printable <'tar> for BoxTabs <'tar> {
 	fn print (self, target: & mut impl Target <'tar>) {
 
 		let border_boxes: Vec <_> = self.tabs.iter ()
@@ -43,6 +43,43 @@ impl <'tar> Printable <'tar> for Tabs <'tar> {
 			target.push (" ");
 			target.push (border_box.bottom ());
 		}
+		target.newline ();
+
+	}
+}
+
+pub struct SlimTabs <'dat> {
+	background: Attr,
+	tabs: Vec <(Cow <'dat, str>, Attr)>,
+}
+
+impl <'dat> SlimTabs <'dat> {
+	pub fn new (background: Attr, tabs: Vec <(Cow <'dat, str>, Attr)>) -> Self {
+		Self { background, tabs }
+	}
+}
+
+impl <'tar> Printable <'tar> for SlimTabs <'tar> {
+	fn print (self, target: & mut impl Target <'tar>) {
+
+		let mut pos = 0;
+
+		for (label, attr) in self.tabs {
+			pos += label.chars ().count () + 3;
+			target.push (self.background);
+			target.push (" ");
+			target.push (attr);
+			target.push (" ");
+			target.push (label);
+			target.push (" ");
+		}
+
+		target.push (self.background);
+		while pos < target.width () {
+			target.push (" ");
+			pos += 1;
+		}
+
 		target.newline ();
 
 	}
